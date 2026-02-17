63f083c02f7283d506ef1b0654cbed8b22328348
Oghenemarho Orukele <orukele.dev@gmail.com>
2026-02-17 08:37:49 +0100

    added data and able to read it

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <sstream>
+#include <format>
+#include <print>
+
+std::string readFile(const std::filesystem::path& path)
+{
+    std::ifstream file{path};
+    // check if file is opened properly
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Failed to open file: " + path.string());
+    }
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    const std::filesystem::path path{"./enron1/ham/0002.1999-12-13.farmer.ham.txt"};
+    std::string content; // NOTE: file is read properly but printing doesn't show all contents of the file
+
+    try
+    {
+        content = readFile(path);
+    } catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    std::cout << content << '\n';
+
     return 0;
 }
